97c974c91f1a23aad00702c808b1a243215c8ed8
AlexeyChernyshkov <alchern1999@gmail.com>
2024-03-03 20:36:50 +0700

    + Изменил url

--- SuperJ❤b/config.py
+++ SuperJ❤b/config.py
@@ -1,18 +1,16 @@
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-# Настройка опций для headless режима
+
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
-# Инициализация WebDriver с указанием опций
-driver = webdriver.Chrome(options=chrome_options)
+
 base_url = 'https://www.superjob.ru/'
 
-search_result_vakansii_url = 'https://www.superjob.ru/vacancy/search/'
-search_result_resume_url = 'https://www.superjob.ru/resume/search_resume.html'
+search_result_vacancy_url = 'https://www.superjob.ru/vacancy/search/'
+search_result_resumes_url = 'https://www.superjob.ru/resume/search_resume.html'
 search_result_clients_url = 'https://russia.superjob.ru/clients/'
-search_result_kursy_url = 'https://www.superjob.ru/kursy/'
+search_result_courses_url = 'https://www.superjob.ru/kursy/'
 
 user_resume_url = 'https://www.superjob.ru/user/resume/'
 resume_create_url = 'https://www.superjob.ru/resume/create/'
@@ -23,3 +21,4 @@ detail_client_page_url = 'https://russia.superjob.ru/clients/'
 detail_course_page_url = 'https://www.superjob.ru/kursy/'
 
 employer_login_url = 'https://www.superjob.ru/auth/login/'
+
