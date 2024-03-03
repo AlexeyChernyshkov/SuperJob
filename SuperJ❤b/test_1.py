31cc1b56fbb0d7bb0cd283c52cc5cfd745937ff9
AlexeyChernyshkov <alchern1999@gmail.com>
2024-03-03 20:47:56 +0700

    + Пробелы

--- SuperJ❤b/test_1.py
+++ SuperJ❤b/test_1.py
@@ -4,6 +4,7 @@ import pytest
 import time
 from pages.locators import *
 
+
 @pytest.fixture()
 def browser():
     driver = webdriver.Chrome(options=chrome_options)
@@ -11,6 +12,7 @@ def browser():
     driver.implicitly_wait(5)
     yield driver
     driver.quit()
+
 
 def test_open_page(browser):
     # create_resume_button = browser.find_element(By.XPATH, '//*[@id="app"]/div/div[1]/div[4]/div/div[1]/div[2]/div/div[1]/div/a/span')
