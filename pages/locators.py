1caf90f9c2f97a8de04e89098e0ac92d3805ecd4
AlexeyChernyshkov <alchern1999@gmail.com>
2024-03-10 21:42:11 +0700

    + Добавил локаторы

--- pages/locators.py
+++ pages/locators.py
@@ -20,12 +20,56 @@ selection_selector = (By.XPATH, '//button[@id="searchByHintSelect-input"]')
 # ---------------------------Страница с резюме в ЛК---------------------------------
 create_resume = (By.XPATH, '//*[@id="app"]/div/div[1]/div[5]/div/div[1]/div/div[2]/a[1]/span')
 
-# Страница создания резюме
-import_resume = (By.XPATH, '//*[@id="app"]/div/div[1]/div[5]/div/div/div[1]/div/div[1]/div/div/div[1]/button/div/span')
+# ---------------------------Страница создания резюме---------------------------------
+import_resume = (By.CSS_SELECTOR, '.f-test-button-Import_rezyume')
 field_name = By.NAME, 'person.firstName'
 field_lastname = (By.NAME, 'person.lastName')
 field_birthdate = (By.CSS_SELECTOR, 'input[name="birthDate"]')
 calendar_birthday = (By.CSS_SELECTOR, '[class="_1yghA WwHMf _2PlM0"]')
+hide_birthday_checkbox = (By.NAME, 'hideBirthday')
+add_photo = (By.CSS_SELECTOR, 'span > button> span')
+add_photo_img = (By.CSS_SELECTOR, 'span > div img')
+town = (By.CSS_SELECTOR, '[name="town.id"]:not(.f-test-block-experience [name="town.id"])')
+#remove_town =
+remote_work_checkbox = (By.NAME, 'remoteWork')
+business_trip_checkbox = (By.NAME, 'businessTrip')
+relocate_possibility_checkbox = (By.NAME, 'relocatePossibility')
+phone = (By.NAME, 'phone')
+email = (By.NAME, 'contacts.email')
+add_contacts = (By.CSS_SELECTOR, '.f-test-button-Dobavit_kontakty_socseti')
+job_position = (By.NAME, 'position')
+salary = (By.NAME, 'salary')
+work_type = (By.ID, 'workType.id-input')
+volunteer_work = (By.NAME, 'volunteerWork')
+
+# Сохранение резюме
+resume_save_button = (By.CSS_SELECTOR, '.f-test-button-Sohranit:not(.f-test-block-experience .f-test-button-Sohranit)')
+rule_agreement_checkbox = (By.NAME, 'rulesAgreement')
+change_user_agreement_text = (By.CSS_SELECTOR, '.f-test-pseudolink-Izmenit_usloviya_i_zaprety_na_obrabotku_personalnyh_dannyh')
+sign_in_button = (By.CSS_SELECTOR, '.f-test-button-Vojti') # только у неавторизованного юзера
+
+# Раздел "Опыт работы"
+experience_position = (By.NAME, 'experience.position')
+experience_company_title = (By.NAME, 'resumeCompany.title')
+experience_company_site = (By.NAME, 'resumeCompany.website')
+experience_company_town = (By.CSS_SELECTOR, '.f-test-block-experience [name="town.id"]')
+#experience_company_town_remove =
+experience_company_activity = (By.NAME, 'resumeCompany.description')
+experience_start_month = (By.CSS_SELECTOR, '[name="dateStart"] input[name="month"]')
+experience_start_year = (By.CSS_SELECTOR, '[name="dateStart"] input[name="year"]')
+experience_end_month = (By.CSS_SELECTOR, '[name="dateEnd"] input[name="month"]')
+experience_end_year = (By.CSS_SELECTOR, '[name="dateEnd"] input[name="year"]')
+experience_still_work = (By.NAME, 'stillWorking')
+experience_responsibility = (By.NAME, 'responsibility')
+experience_achievements = (By.NAME, 'achievements')
+no_work_experience_checkbox = (By.NAME, 'noWorkExperience')
+experience_save_button = (By.CSS_SELECTOR, '.f-test-block-experience .f-test-button-Sohranit')
+
+# Раздел с доп. параметрами
+
+
+
+
 
 # --------------------------Страница поиска вакансий---------------------------------
 button_filter_search = (By.XPATH, '//button[@title="Расширенный поиск"]')
