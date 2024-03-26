41d918561ec6ab4aae255d1381e250095bc4748b
AlexeyChernyshkov <alchern1999@gmail.com>
2024-03-27 00:16:28 +0700

    + Изменил названия локаторов авторизации в соответствии с изменениями в locators.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,18 +1,18 @@
-from locators import *
-from users import *
+from pages.locators import *
+from pages.users import *
 
 
 # Класс для авторизации пользователем
 class Autorization_customers:
-    def __init__(self, driver):
-        self.field_username = driver.find_element(*field_username)
-        self.field_password = driver.find_element(*field_password)
-        self.button_login = driver.find_element(*button_login)
+    def __init__(self, browser):
+        self.field_username = browser.find_element(*field_username)
+        self.field_password = browser.find_element(*field_password)
+        self.button_login_accept = browser.find_element(*button_login_accept)
 
     def login(self, field_username: str = '', field_password: str = ''):  # авторизация или вход на страницу
         self.field_username.send_keys(test_user_customers)  # заполнение поля логин
         self.field_password.send_keys(test_password_user)  # заполнение поля пароль
-        self.button_login.click()  # нажатие на кнопку Войти
+        self.button_login_accept.click()  # нажатие на кнопку Войти
 
 
 #  Класс для авторизации компанией
@@ -20,9 +20,9 @@ class Autorization_ccompany:
     def __init__(self, driver):
         self.field_username = driver.find_element(*field_username)
         self.field_password = driver.find_element(*field_password)
-        self.button_login = driver.find_element(*button_login)
+        self.button_login_accept = driver.find_element(*button_login_accept)
 
     def login(self, field_username: str = '', field_password: str = ''):  # авторизация или вход на страницу
         self.field_username.send_keys(test_user_company_1)  # заполнение поля логин
         self.field_password.send_keys(test_password_user_company)  # заполнение поля пароль
-        self.button_login.click()  # нажатие на кнопку Войти
+        self.button_login_accept.click()  # нажатие на кнопку Войти
