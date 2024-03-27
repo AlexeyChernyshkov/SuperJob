df18a059a8469b1b35b5d1a5192be812903de1ea
AlexeyChernyshkov <alchern1999@gmail.com>
2024-03-28 00:49:30 +0700

    + Добавил тест заполнения обязательных полей + Добавил тест заполнения всех полей

--- tests/test_resume_create_page.py
+++ tests/test_resume_create_page.py
@@ -2,6 +2,7 @@ import pytest
 
 from pages.resume_create import *
 from pages.main_page import Autorization_customers
+from pages.base_page import BasePage
 from pages.users import *
 
 import time
@@ -41,15 +42,71 @@ import time
 #     assert resume_create.change_user_agreement_text().is_displayed, f"No change_user_agreement_text"
 #     # assert resume_create.sign_in_button().is_displayed, f"No sign_in_button"
 
+# @pytest.mark.parametrize('username, password', [(test_user_customers, test_password_user)])
+# def test_resume_create_with_authorization_required_fields_only(browser, username, password):
+#     resume_auth_create_1 = ResumeCreate(browser, resume_create_url)
+#     resume_auth_create_1.open()
+#     resume_auth_create_1.top_button_login().click()
+#
+#     # Ожидаем открытия окна авторизации
+#     resume_auth_create_1.wait_text_in_element(button_login_accept, "Войти")
+#
+#     # Авторизуемся методом из main_pages
+#     authorization_1 = Autorization_customers(browser)
+#     authorization_1.login(username, password)
+#
+#     # Ожидаем переход к экрану создания резюме после авторизации
+#     resume_auth_create_1.wait_text_in_element(resume_save_button, "Сохранить")
+#
+#     resume_auth_create_1.job_position().send_keys("Тестировщик")
+#     resume_auth_create_1.wait_text_in_element(job_dropdown, "Тестировщик")
+#     resume_auth_create_1.job_dropdown().click()
+#     # resume_auth_create.resume_save_button().click()
+
 @pytest.mark.parametrize('username, password', [(test_user_customers, test_password_user)])
-def test_resume_create_with_authorization(browser, username, password):
-    resume_auth_create = ResumeCreate(browser, resume_create_url)
-    resume_auth_create.open()
-    resume_auth_create.top_button_login().click()
-    resume_auth_create.wait_text_in_element(button_login_accept, "Войти")
-    authtorization = Autorization_customers(browser)
-    authtorization.login(username, password)
-    time.sleep(3)
+def test_resume_create_with_authorization_all_fields(browser, username, password):
+    resume_auth_create_2 = ResumeCreate(browser, resume_create_url)
+    resume_auth_create_2.open()
+    resume_auth_create_2.top_button_login().click()
+
+    # Ожидаем открытия окна авторизации
+    resume_auth_create_2.wait_text_in_element(button_login_accept, "Войти")
+
+    # Авторизуемся методом из main_pages
+    authorization_2 = Autorization_customers(browser)
+    authorization_2.login(username, password)
+
+    # Ожидаем переход к экрану создания резюме после авторизации
+    resume_auth_create_2.wait_element_to_be_clickable(resume_save_button)
+
+    resume_auth_create_2.birthday().send_keys("15.12.1999")
+
+    """Уперся в клик по чек-боксу
+    # resume_auth_create_2.check_box_click(hide_birthday_checkbox)
+    resume_auth_create_2.hide_birthday_checkbox().click()
+    # resume_auth_create_2.remote_work_checkbox().click()
+    # resume_auth_create_2.business_trip_checkbox().click()
+    # resume_auth_create_2.relocate_possibility_checkbox().click()
+    """
+
+    # Открываем окно добавления фото через кнопку
+    resume_auth_create_2.add_photo().click()
+    resume_auth_create_2.wait_element_to_be_clickable(add_photo_cancel)
+    resume_auth_create_2.add_photo_cancel().click()
+
+    # Открываем окно добавления фото через изображение
+    resume_auth_create_2.add_photo_img().click()
+    resume_auth_create_2.wait_element_to_be_clickable(add_photo_cancel)
+    resume_auth_create_2.add_photo_cancel().click()
+
+    resume_auth_create_2.job_position().send_keys("Тестировщик")
+    resume_auth_create_2.wait_text_in_element(job_dropdown, "Тестировщик")
+    resume_auth_create_2.salary().send_keys("100000")
+    resume_auth_create_2.work_type().click()
+    resume_auth_create_2.work_type_dropdown_full().click()
+
+    time.sleep(5)
+
 
 # def test_resume_completion(browser):
 #     resume_completion = ResumeCreate(browser, resume_create_url)
