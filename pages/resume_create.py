d9c248786e8d3486149f3019bf6e29ecee8329ab
AlexeyChernyshkov <alchern1999@gmail.com>
2024-03-30 19:01:12 +0700

    + Добавил методы для элементов опыта работы

--- pages/resume_create.py
+++ pages/resume_create.py
@@ -37,6 +37,15 @@ class ResumeCreate(BasePage):
     def add_photo_cancel(self):
         return self.find(add_photo_cancel)
 
+    def add_photo_close(self):
+        return self.find(add_photo_close)
+
+    def add_photo_upload(self):
+        return self.find(add_photo_upload)
+
+    def add_photo_save(self):
+        return self.find(add_photo_save)
+
     def town(self):
         return self.find(town)
 
@@ -93,3 +102,75 @@ class ResumeCreate(BasePage):
 
     def sign_in_button(self):
         return self.find(sign_in_button)
+
+    def experience_position(self):
+        return self.find(experience_position)
+
+    def experience_position_dropdown(self):
+        return self.find(experience_position_dropdown)
+
+    def experience_company_title(self):
+        return self.find(experience_company_title)
+
+    def experience_company_title_dropdown(self):
+        return self.find(experience_company_title_dropdown)
+
+    def experience_company_site(self):
+        return self.find(experience_company_site)
+
+    def experience_company_town(self):
+        return self.find(experience_company_town)
+
+    def experience_company_town_dropdown(self):
+        return self.find(experience_company_town_dropdown)
+
+    def experience_company_town_remove(self):
+        return self.find(experience_company_town_remove)
+
+    def experience_company_activity(self):
+        return self.find(experience_company_activity)
+
+    def experience_start_month(self):
+        return self.find(experience_start_month)
+
+    def experience_start_month_dropdown(self):
+        return self.find(experience_start_month_dropdown)
+
+    def experience_start_year(self):
+        return self.find(experience_start_year)
+
+    def experience_start_year_dropdown(self):
+        return self.find(experience_start_year_dropdown)
+
+    def experience_end_month(self):
+        return self.find(experience_end_month)
+
+    def experience_end_month_dropdown(self):
+        return self.find(experience_end_month_dropdown)
+
+    def experience_end_year(self):
+        return self.find(experience_end_year)
+
+    def experience_end_year_dropdown(self):
+        return self.find(experience_end_year_dropdown)
+
+    def experience_still_work(self):
+        return self.find(experience_still_work)
+
+    def experience_still_work_click(self, *args):
+        return self.script_click(args)
+
+    def experience_responsibility(self):
+        return self.find(experience_responsibility)
+
+    def experience_achievements(self):
+        return self.find(experience_achievements)
+
+    def no_work_experience_checkbox(self):
+        return self.find(no_work_experience_checkbox)
+
+    def experience_save_button(self):
+        return self.find(experience_save_button)
+
+
+
