ff65edd70341c8dd23ba0c7aac37ac56eee31ac7
AlexeyChernyshkov <alchern1999@gmail.com>
2024-03-30 19:00:46 +0700

    + Добавил локаторы

--- pages/locators.py
+++ pages/locators.py
@@ -7,8 +7,10 @@ top_button_login = (By.CSS_SELECTOR, '.f-test-link-Vhod')
 #  Поля логин и пароль
 field_username = (By.XPATH, '//input[@name="login"]')
 field_password = (By.XPATH, '//input[@name="password"]')
+
 # Кнопка Войти
-button_login_accept = (By.XPATH, '//button[@class="_129Bc _1L7ad ejaLn PjivS f-test-button-Vojti _26xin"]')
+# button_login_accept = (By.XPATH, '//button[@class="_129Bc _1L7ad ejaLn PjivS f-test-button-Vojti _26xin"]')
+button_login_accept = (By.CSS_SELECTOR, '.f-test-button-Vojti')
 
 # Строка поиска, у резюме своя строка
 the_search_string = (By.XPATH, '//input[@name="keywords"]')
@@ -28,12 +30,17 @@ field_lastname = (By.NAME, 'person.lastName')
 field_birthdate = (By.CSS_SELECTOR, 'input[name="birthDate"]')
 calendar_birthday = (By.CSS_SELECTOR, '[class="_1yghA WwHMf _2PlM0"]')
 hide_birthday_checkbox = (By.NAME, 'hideBirthday')
+
 add_photo = (By.CSS_SELECTOR, '.f-test-button-Dobavit_foto')
-# add_photo_img = (By.CSS_SELECTOR, 'span > div img')
 add_photo_img = (By.CSS_SELECTOR, '.f-test-clickable-Dobavit_foto')
+
 add_photo_cancel = (By.CSS_SELECTOR, 'button.f-test-button-Otmenit')
+add_photo_close = (By.CSS_SELECTOR, '.f-test-button-close')
+add_photo_upload = (By.NAME, 'avatar_upload_field')
+add_photo_save = (By.CSS_SELECTOR, '#avatarUpload .f-test-button-Sohranit')
+
 town = (By.CSS_SELECTOR, '[name="town.id"]:not(.f-test-block-experience [name="town.id"])')
-town_dropdown = (By.CSS_SELECTOR, '.f-test-option-Barnaul')
+town_dropdown = (By.CSS_SELECTOR, '[class*="dropdown-town"] span:nth-child(1) button')
 remove_town = (By.CSS_SELECTOR, ':not(.f-test-block-experience .undefined) [data-form-id="RESUME_GENERAL_INFO_FORM"] label[for="town.id"] .undefined > span ')
 remote_work_checkbox = (By.NAME, 'remoteWork')
 business_trip_checkbox = (By.NAME, 'businessTrip')
@@ -42,7 +49,7 @@ phone = (By.NAME, 'phone')
 email = (By.NAME, 'contacts.email')
 add_contacts = (By.CSS_SELECTOR, '.f-test-button-Dobavit_kontakty_socseti')
 job_position = (By.NAME, 'position')
-job_dropdown = (By.CSS_SELECTOR, '.f-test-option-Testirovschik')
+job_dropdown = (By.CSS_SELECTOR, '[class*="dropdown-position"] span:nth-child(1) button')
 salary = (By.NAME, 'salary')
 work_type = (By.ID, 'workType.id-input')
 work_type_dropdown_full = (By.ID, "6")
@@ -56,16 +63,30 @@ sign_in_button = (By.CSS_SELECTOR, '.f-test-button-Vojti')  # только у н
 
 # Раздел "Опыт работы"
 experience_position = (By.NAME, 'experience.position')
+experience_position_dropdown = (By.CSS_SELECTOR, '[class*="dropdown-experience"] span:nth-child(1) button')
+
 experience_company_title = (By.NAME, 'resumeCompany.title')
+experience_company_title_dropdown = (By.CSS_SELECTOR, '[class*="dropdown-resumeCompany"] span:nth-child(1) button')
+
 experience_company_site = (By.NAME, 'resumeCompany.website')
+
 experience_company_town = (By.CSS_SELECTOR, '.f-test-block-experience [name="town.id"]')
-experience_company_town_remove = (By.CSS_SELECTOR, '.f-test-block-experience .undefined > span')
+experience_company_town_dropdown = (By.CSS_SELECTOR, '[class*="dropdown-town"] span:nth-child(1) button')
+experience_company_town_remove = (By.CSS_SELECTOR, '.f-test-block-experience [for="town.id"] .undefined > span')
+
 experience_company_activity = (By.NAME, 'resumeCompany.description')
+
 experience_start_month = (By.CSS_SELECTOR, '[name="dateStart"] input[name="month"]')
+experience_start_month_dropdown = (By.CSS_SELECTOR, '[class*="dropdown-month"] span:nth-child(1) button')
 experience_start_year = (By.CSS_SELECTOR, '[name="dateStart"] input[name="year"]')
+experience_start_year_dropdown = (By.CSS_SELECTOR, '[class*="dropdown-year"] span:nth-child(1) button')
+
 experience_end_month = (By.CSS_SELECTOR, '[name="dateEnd"] input[name="month"]')
+experience_end_month_dropdown = (By.CSS_SELECTOR, '[class*="dropdown-month"] span:nth-child(1) button')
 experience_end_year = (By.CSS_SELECTOR, '[name="dateEnd"] input[name="year"]')
-experience_still_work = (By.NAME, 'stillWorking')
+experience_end_year_dropdown = (By.CSS_SELECTOR, '[class*="dropdown-year"] span:nth-child(1) button')
+
+experience_still_work = (By.ID, 'stillWorking')
 experience_responsibility = (By.NAME, 'responsibility')
 experience_achievements = (By.NAME, 'achievements')
 no_work_experience_checkbox = (By.NAME, 'noWorkExperience')
